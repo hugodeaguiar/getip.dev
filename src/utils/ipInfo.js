4e45b4e683059384759102a32e930f128642063b
Marcos Marcão Aurelio <macosaures+github+git@gmail.com>
2025-01-31 18:30:31 -0300

    Refactor homeController and ipInfo utility to enhance IP data retrieval and structure response data

--- src/utils/ipInfo.js
+++ src/utils/ipInfo.js
@@ -1,36 +1,53 @@
 import https from 'https';
 
 export function getIPInfo(callback, ip) {
-    const options = {
-        hostname: 'ipinfo.io',
-        port: 443,
-        path: '/' + ip + '/?token=' + process.env.GETINFO_TOKEN,
-        method: 'GET',
-        timeout: 3000 // Timeout in milliseconds (3 seconds)
+    const getIPData = (version) => {
+        return new Promise((resolve, reject) => {
+            const path = version === 'ipv6' ? `/${ip}/json?token=${process.env.GETINFO_TOKEN}` : `/${ip}/?token=${process.env.GETINFO_TOKEN}`;
+            console.log(path)
+            const options = {
+                hostname: 'ipinfo.io',
+                port: 443,
+                path: path,
+                method: 'GET',
+                timeout: 3000
+            };
+
+            const req = https.request(options, (resp) => {
+                let data = '';
+
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+
+                resp.on('end', () => {
+                    resolve(data);
+                });
+            });
+
+            req.on('timeout', () => {
+                console.error('Request timed out');
+                req.abort();
+                reject(new Error('Request timed out'));
+            });
+
+            req.on('error', (err) => {
+                console.error("Error: " + err.message);
+                reject(err);
+            });
+
+            req.end();
+        });
     };
 
-    const req = https.request(options, (resp) => {
-        let data = '';
-
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-            data += chunk;
+    Promise.all([getIPData('ipv4'), getIPData('ipv6')])
+        .then(([ipv4Data, ipv6Data]) => {
+            const ipInfo = ipv4Data ? ipv4Data : ipv6Data;
+            callback(ipInfo);
+        })
+        .catch((err) => {
+            console.error("Error: " + err.message);
+            callback(null, err);
         });
-
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            callback(data);
-        });
-    });
-
-    req.on('timeout', () => {
-        console.error('Request timed out');
-        req.abort(); // Aborts the request
-    });
-
-    req.on('error', (err) => {
-        console.error("Error: " + err.message);
-    });
-
-    req.end();
 }
